1b7fdf6578aaef1d28a5da6c61373dffd215e4fb
ranjanichandrsekar-byte <ranjanichandrsekar@gmail.com>
2026-05-05 12:09:40 +0530

    1st commit

---
+++ cedcab/dbcon.php
@@ -0,0 +1,25 @@
+<?php
+    class dbcon
+    {
+
+    public $dbhost;
+    public $dbuser;
+    public $dbpass;
+    public $dbname;
+    public $server;
+    public $db;
+    public $conn;
+
+    function __construct(){
+
+    $this->conn = new mysqli("localhost", "root", "", "cedcab");
+
+       
+
+    if ($this->conn->connect_error) {
+    die("Connection failed: " . $this->conn->connect_error);
+    }
+    }
+
+    }
+    ?>
